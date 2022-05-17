75d2d75b76b23741a6f9c41a45d45a5cd72ff866
Ildar-Bart <ildar_kgbi@mail.ru>
2022-05-18 04:00:12 +0500

    header

--- js/mod.js
+++ js/mod.js
@@ -1,20 +1,20 @@
 
 // бургер
-document.querySelector(".header__btn-open").addEventListener("click", function() {
+document.querySelector(".header__burger").addEventListener("click", function() {
   document.querySelector(".header__nav").classList.add("active");
 })
-document.querySelector(".header__btn-close").addEventListener("click", function() {
+document.querySelector(".header__burger-close").addEventListener("click", function() {
   document.querySelector(".header__nav").classList.remove("active");
 })
 
 
 // поиск
-document.querySelector(".header__btn-search").addEventListener("click", function() {
-  document.querySelector(".header__form").classList.add("form__active");
+document.querySelector(".header__btn-search-open").addEventListener("click", function() {
+  document.querySelector(".header__search").classList.add("form__active");
   this.classList.add("active");
 })
 
 document.querySelector(".header__form-btn-close").addEventListener("click", function() {
-  document.querySelector(".header__form").classList.remove("form__active");
+  document.querySelector(".header__search").classList.remove("form__active");
   document.querySelector(".header__form-btn-close").classList.remove("active")
 });
