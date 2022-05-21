7f12967bad185d56f686c5a1405217b6c0c2b7cd
Ildar-Bart <ildar_kgbi@mail.ru>
2022-05-21 23:08:33 +0500

    модальное окно готова

--- js/modal-window.js
+++ js/modal-window.js
@@ -1,15 +1,15 @@
 const btnmw = document.querySelectorAll('.btn-modal');
 const modalOverlay = document.querySelector('.modal-overlay')
-const modals = document.querySelectorAll('.modals')
+const modals = document.querySelectorAll('.modal')
 const btnclose = document.querySelectorAll('.modal__window-close-btn')
 
 btnmw.forEach((el) => {
   el.addEventListener('click', (e) => {
     let path = e.currentTarget.getAttribute('data-path');
 
-    // modals.forEach((el) => {
-    //   el.classList.remove('modal--visible')
-    // });
+    modals.forEach((el) => {
+      el.classList.remove('modal--visible')
+    });
 
     document.querySelector(`[data-target="${path}"]`).classList.add('modal--visible')
     modalOverlay.classList.add('modal-overlay--visible');
@@ -17,16 +17,15 @@ btnmw.forEach((el) => {
 });
 
 
-// btnclose.addEventListener('click', (e) => {
-//   modals.forEach((el) => {
-//     el.classList.remove('modal--visible')
-//   });
-//   btnclose.classList.remove('modal-overlay--visible');
+btnclose.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    modals.forEach((el) => {
+      modalOverlay.classList.remove('modal-overlay--visible')
+    });
+    modals.forEach((el) => {
+      el.classList.remove('modal--visible')
+    });
 
-// })
-
-
-document.querySelector(".modal__window-close-btn").addEventListener("click", function() {
-  document.querySelector(".modal").classList.remove("modal--visible");
-  document.querySelector(".modal-overlay").classList.remove("modal-overlay--visible")
 });
+})
+
